105845d52fef5de3389e14382ed332892efc7db2
Robert Radzville <rradzvil@ucsc.edu>
2020-02-28 15:46:19 -0800

    Brick works

--- BreakoutBeta/Brick.gd
+++ BreakoutBeta/Brick.gd
@@ -1,11 +1,8 @@
 extends Sprite
 
-onready var area2d = get_node("Area2D")
-
 func _ready():
-	area2d.connect("body_enter", self, "_on_brick_body_enter")
-	
-func _on_brick_body_enter(body):
-	#if the ball hit the brick, destroy it
+	pass
+
+func _on_Area2D_body_entered(body):
 	if body.get_name() == "Ball":
-			queue_free()
+		queue_free()
