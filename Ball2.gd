d753581c08ddb3031f6de8e15ba8cb285eafba68
Katze-RGB <60154246+Katze-RGB@users.noreply.github.com>
2020-03-05 10:11:29 -0800

    Update Ball2.gd
    actually works

--- Ball2.gd
+++ Ball2.gd
@@ -1,7 +1,4 @@
 extends RigidBody2D
-const BOOST = 4
-const MAX = 300
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -11,20 +8,14 @@ const MAX = 300
 func _ready():
 	set_process(true)
 
-func _process(delta):
-	var bodies = get_colliding_bodies()
-	
-	for body in bodies:
-		#if position.y > get_viewport_rect().end.y:
-		#	queue_free()
-		if body.get_name() == "CollisionShape2D2":
-			get_node("Ball").set_global_position(50,200)
-		if body.get_name() == "Paddle":
-			var speed = linear_velocity.length()
-			var direction = position - body.get_node("Ancor").global_position
-			var velocity = direction * min(speed + BOOST * delta, MAX * delta)
-			linear_velocity = velocity
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _integrate_forces(state):
+	var resetpos= Vector2(50,200)
+	var xform = state.get_transform()
+	if position.y > get_viewport_rect().end.y:
+		print("Ball died")
+		xform.origin.y=(50)
+		xform.origin.x=(200)
+		state.set_transform(xform)
+		#set_sleeping(true)
+		
+		#set_sleeping(false)
