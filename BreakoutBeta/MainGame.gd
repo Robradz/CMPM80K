949d20eb4eb1f2d59f39d24773a31a8e0286c4f1
Robert Radzville <rradzvil@ucsc.edu>
2020-03-05 17:21:19 -0800

    Start Screen and more bricks

---
+++ BreakoutBeta/MainGame.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+var numlives = 3
+var num_bricks = 36
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process(true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
